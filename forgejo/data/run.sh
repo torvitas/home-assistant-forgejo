5cac887424c4b2e149a05a887d7cd4e4070338a4
Schmidt, Sascha (sasschmidt) <sascha.schmidt@breuninger.de>
2026-01-06 19:24:15 +0100

    fix: Actually run the setup

--- forgejo/data/run.sh
+++ forgejo/data/run.sh
@@ -6,4 +6,5 @@ bashio::log.info "Starting forgejo..."
 
 chown git:git "$GITEA_CUSTOM" "${GITEA_TEMP}"
 
+exec gosu git /usr/local/bin/docker-setup.sh
 exec gosu git /app/gitea/gitea $(bashio::config "arguments")
